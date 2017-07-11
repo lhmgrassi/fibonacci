5e7822f1494daf6a026c895fa999facef9da3d3b
Luis Henrique Grassi <lhmgrassi@gmail.com>
2017-07-11 06:07:14 -0300

    removed boilerplate code

---
+++ Fibonacci/Fibonacci/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Fibonacci
+//
+//  Created by Luis Henrique Grassi on 11/07/17.
+//  Copyright © 2017 lhmgrassi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
